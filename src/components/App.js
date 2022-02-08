daf114958c82f1f004fc56e0a8260e348e33eec7
tasrifkh <tasrif.me@gmail.com>
2022-02-08 15:56:31 +0600

    data map in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Card from './Card';
+import Data from '../Data.json'
 
 function App() {
+
+    let items = [];
+    items = Data.map((item,index) => <Card key = {index} titleText={item.title} descText = {item.desc} />);
+    // for (let x= 0; x< Data.length; x++){
+    //     items.push( <Card titleText={Data[x].title} descText = {Data[x].desc}/>)
+    // }
     return <div>
-<h1 className = "headingStyle">Todo App</h1>
-        
-        <Card titleText="hello" descText = "hi"  />
-        <Card titleText="hello2" descText = "hi2"  />
-        <Card titleText="hello3" descText = "hi3"  />
-        
+      <h1 className = "headingStyle">Todo App</h1>
+      {items}
+
     </div>
 
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(
 
 
   <div>
-  
+   
       <App />
   </div>,document.getElementById('root'));
 
