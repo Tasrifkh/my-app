4c5f2e3eb1cf8ea052217f2de5326c1f8d236a4e
tasrifkh <tasrif.me@gmail.com>
2022-02-22 18:06:40 +0600

    State started

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,30 @@
 import React from "react";
+import STATE from "../STATE";
 
 //card 1 and 2
-import Card1 from "./Card1";
-import Card2 from "./Card2";
+// import Card1 from "./Card1";
+// import Card2 from "./Card2";
 
 // import Card from './Card';
 // import Data from '../Data.json'
 
 function App() {
 
-    //.Card data load...
+   
+  //.Card data load...
   
     // let items = [];
     // items = Data.map((item,index) => <Card key = {index} titleText={item.title} descText = {item.desc} />);
     // for (let x= 0; x< Data.length; x++){
     //     items.push( <Card titleText={Data[x].title} descText = {Data[x].desc}/>)
     // }
-    return <div>
-      
-     <Card1 name="Hello" desc = "Test"/>
-     <Card2 name="Hello" desc = "Test"/>
+   
+   return <div>
+      <STATE/>
+     
+     
+     {/* <Card1 name="Hello" desc = "Test"/>
+     <Card2 name="Hello" desc = "Test"/> */}
       {/* Card data load */}
      {/* <h1 className = "headingStyle">Todo App</h1> 
       {items} */}
